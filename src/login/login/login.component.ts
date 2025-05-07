65d707fdd543560009e59fd54395f50fa55f83ab
GaborHozleiter <154556447+GaborHozleiter@users.noreply.github.com>
2025-05-07 13:50:42 +0200

    add basic login form with reactive validation

---
+++ src/login/login/login.component.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-login',
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent {
+  isLoading: boolean = false;
+
+  applyForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+    rememberMe: new FormControl(''),
+  });
+
+  submitLogin() {
+    this.isLoading = true;
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 2000);
+    console.log(this.applyForm.value);
+    this.applyForm.reset();
+  }
+}
