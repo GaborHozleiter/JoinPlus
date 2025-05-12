38066e8e552792615507f441bc8bbe10c5eab9b2
GaborHozleiter <154556447+GaborHozleiter@users.noreply.github.com>
2025-05-12 23:51:31 +0200

    added home component structur

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,14 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from '../pages/login/login.component';
 import { ForgotPasswordComponent } from '../pages/forgot-password/forgot-password.component';
 import { RegisterComponent } from '../pages/register/register.component';
+import { HomeComponent } from '../pages/home/home.component';
+import { DashboardComponent } from '../pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
-  { path: '', component: LoginComponent },
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'dashboard', component: DashboardComponent},
+  { path: '**', redirectTo: '' },
 ];
